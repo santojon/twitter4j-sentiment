11db93080dcc3a230fd9c83eafb9ce816059140d
Jonathan Santos <santojon5@gmail.com>
2016-06-22 11:40:13 -0300

    update info acquiring

--- src/main/java/com/twitter4j/sentiment/App.java
+++ src/main/java/com/twitter4j/sentiment/App.java
@@ -1,50 +1,91 @@
 package com.twitter4j.sentiment;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class App {
 
     public static void main(String[] args) {
         String topic = "Restaurants";
         
-        ArrayList<String> tweets = TweetManager.getTweets(topic);
+        HashMap<String, ArrayList<ArrayList<String>>> tweets = TweetManager.getTweets(topic);
         NLP.init();
         
         int total = tweets.size();
         int good = 0, bad = 0, neutral = 0;
         
-        for(String tweet : tweets) {
+        for(String tweet : tweets.keySet()) {
+        	System.out.println();
+        	System.out.println("-------------------------- TWEET --------------------------");
+        	System.out.println(tweet);
+        	System.out.println();
+        	
+        	// Extra info (mentions, hashtags, urls)
+        	ArrayList<ArrayList<String>> info = tweets.get(tweet);
+        	
+        	// Mentions
+        	if (info.get(0).size() > 0) {
+        		System.out.println("---------- MENTIONS ---------");
+        		for (String mention : info.get(0)) {
+        			System.out.println(mention);
+        		}
+        		System.out.println();
+        	}
+        	
+        	// Hastags
+        	if (info.get(1).size() > 0) {
+        		System.out.println("---------- HASTAGS ---------");
+        		for (String tag : info.get(1)) {
+        			System.out.println(tag);
+        		}
+        		System.out.println();
+        	}
+        	
+        	// URLs
+        	if (info.get(2).size() > 0) {
+        		System.out.println("---------- URLS ---------");
+        		for (String url : info.get(2)) {
+        			System.out.println(url);
+        		}
+        		System.out.println();
+        	}
+        	
         	int mainSentiment = NLP.findSentiment(tweet);
         	switch (mainSentiment) {
 	            case 0:
 	            	bad++;
-	            	System.out.println(tweet + " : Very Negative");
+	            	System.out.println("-------------------------- Very Negative -> Negative --------------------------");
 	                break;
 	            case 1:
 	            	bad++;
-	            	System.out.println(tweet + " : Negative");
+	            	System.out.println("-------------------------- Negative --------------------------");
 	                break;
 	            case 2:
 	            	neutral++;
-	            	System.out.println(tweet + " : Neutral");
+	            	System.out.println("-------------------------- Neutral --------------------------");
 	                break;
 	            case 3:
 	            	good++;
-	            	System.out.println(tweet + " : Positive");
+	            	System.out.println("-------------------------- Positive --------------------------");
 	                break;
 	            case 4:
 	            	good++;
-	            	System.out.println(tweet + " : Very Positive");
+	            	System.out.println("-------------------------- Very Positive -> Positive --------------------------");
 	                break;
 	            default:
 	            	neutral++;
-	            	System.out.println(tweet + " : Fail");
+	            	System.out.println("-------------------------- Failure -> Neutral --------------------------");
 	                break;
             }
+        	System.out.println();
         }
         
+        System.out.println();
+        System.out.println();
+        System.out.println("---------- RESULTS ---------");
         System.out.println(total + " tweets analysed.");
         System.out.println(good + " good comments.");
     	System.out.println(bad + " bad comments.");
     	System.out.println(neutral + " neutral comments.");
+    	System.out.println("----------------------------");
     }
 }
--- src/main/java/com/twitter4j/sentiment/TweetManager.java
+++ src/main/java/com/twitter4j/sentiment/TweetManager.java
@@ -1,29 +1,67 @@
 package com.twitter4j.sentiment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
+import twitter4j.HashtagEntity;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.URLEntity;
+import twitter4j.UserMentionEntity;
 
 public class TweetManager {
+	// Max tweets per page (100 is the ceiling!!!)
+	private static final int MAX = 100;
+	
+	// Query language 
+	private static final String LANG = "pt-br";
 
-    public static ArrayList<String> getTweets(String topic) {
+    public static HashMap<String, ArrayList<ArrayList<String>>> getTweets(String topic) {
 
         Twitter twitter = new TwitterFactory().getInstance();
-        ArrayList<String> tweetList = new ArrayList<String>();
+        HashMap<String, ArrayList<ArrayList<String>>> tweetList =
+        		new HashMap<String, ArrayList<ArrayList<String>>>();
         try {
-            Query query = new Query(topic);
+        	// Create and configure query
+            Query query = new Query(topic).count(MAX);
+            query.setLang(LANG);
+            query.setResultType(Query.ResultType.recent);
+            
             QueryResult result;
             do {
                 result = twitter.search(query);
                 List<Status> tweets = result.getTweets();
+                
                 for (Status tweet : tweets) {
-                    tweetList.add(tweet.getText());
+                	tweetList.put(tweet.getText(), new ArrayList<ArrayList<String>>());
+                	ArrayList<ArrayList<String>> lst = tweetList.get(tweet.getText());
+                	
+                	// Mention list
+                	lst.add(new ArrayList<String>());
+                	
+                	// Hashtag list
+                	lst.add(new ArrayList<String>());
+                	
+                	// URL list
+                	lst.add(new ArrayList<String>());
+                	
+                	for (UserMentionEntity mention : tweet.getUserMentionEntities()) {
+                		lst.get(0).add(mention.getText());
+                	}
+                	
+                	for (HashtagEntity tag : tweet.getHashtagEntities()) {
+                		lst.get(1).add(tag.getText());
+                	}
+                	
+                	for (URLEntity url : tweet.getURLEntities()) {
+                		lst.get(2).add(url.getText());
+                	}
                 }
             } while ((query = result.nextQuery()) != null);
         } catch (TwitterException te) {
